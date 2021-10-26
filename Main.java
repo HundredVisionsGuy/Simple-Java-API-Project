e0cd1da4cbad8cd1f09bde5de37d3c2486c1c90a
Chris Winikka <3827026+HundredVisionsGuy@users.noreply.github.com>
2021-10-26 21:47:44 +0000

    Created a successful API call with simple output

--- Main.java
+++ Main.java
@@ -3,8 +3,30 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 
+import org.json.JSONObject;
+
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws Exception {
+    try {
+      // Host URL, endpoint, and query
+      String host = "http://api.icndb.com";
+      String endpoint = "/jokes/random";
+      String query = "?limitTo=[nerdy]";
+
+      // put it all together into the api call
+      String url = host + endpoint + query;
+
+      HttpResponse <JsonNode> response = Unirest.get( url )
+        .asJson(); 
+      
+      // Test the response
+      System.out.println(response.getStatus());
+      System.out.println(response.getBody().toString());
+      
+    }
+
+    catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
