edd58089846d5a621baac437a837e26fa8fec480
Chris Winikka <3827026+HundredVisionsGuy@users.noreply.github.com>
2021-10-27 23:26:04 +0000

    Getting Swing frame to display an image

--- Main.java
+++ Main.java
@@ -2,16 +2,23 @@ import java.net.URLEncoder;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import java.awt.*;
+import javax.swing.JFrame;
 
 import org.json.JSONObject;
 
-class Main {
+class Main extends Canvas {
+  public void paint(Graphics g) {
+    Toolkit t=Toolkit.getDefaultToolkit();
+    Image i=t.getImage("transact-sql.png");
+    g.drawImage(i, 120, 100, this);
+  }
   public static void main(String[] args) throws Exception {
     try {
       // Host URL, endpoint, and query
-      String host = "http://api.icndb.com";
-      String endpoint = "/jokes/random";
-      String query = "?limitTo=[nerdy]";
+      String host = "https://dog.ceo/";
+      String endpoint = "/api/breeds/list/all";
+      String query = "";
 
       // put it all together into the api call
       String url = host + endpoint + query;
@@ -22,6 +29,12 @@ class Main {
       // Test the response
       System.out.println(response.getStatus());
       System.out.println(response.getBody().toString());
+
+      Main m = new Main();
+      JFrame f=new JFrame();
+      f.add(m);
+      f.setSize(400,400);
+      f.setVisible(true);
       
     }
 
