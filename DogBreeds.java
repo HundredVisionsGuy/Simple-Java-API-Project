b564401e4b9fde57375c9cd99f961896789a6ce7
Chris Winikka <3827026+HundredVisionsGuy@users.noreply.github.com>
2021-11-03 23:10:41 +0000

    Playing with Json arrays and objects

---
+++ DogBreeds.java
@@ -0,0 +1,9 @@
+public class DogBreeds {
+  private String message;
+  private String status;
+
+  public DogBreeds(String message, String status) {
+    this.message = message;
+    this.status = status;
+  }
+}
--- Main.java
+++ Main.java
@@ -2,8 +2,24 @@ import java.net.URLEncoder;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.async.Callback;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
+// We need to import gson package from google
+// imports below for parsing JSON
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+// Let's try org.json
 import org.json.JSONObject;
+import org.json.JSONArray;
+
+// import json-simple api
+// use https://www.geeksforgeeks.org/parse-json-java/ for our reference
 
 class Main {
   public static void main(String[] args) throws Exception {
@@ -16,12 +32,28 @@ class Main {
       // put it all together into the api call
       String url = host + endpoint + query;
 
-      HttpResponse <JsonNode> response = Unirest.get( url )
-        .asJson(); 
+      // Let's make the call & store result as a string
+      String result = Unirest.get(url)
+          .asJson()
+          .getBody()
+          .getObject()
+          .toString();
+
+      //System.out.println(result + "\n\n\n");
+      result = "[" + result + "]";
+
+      // Let's try a JSONArray
+      JSONArray array = new JSONArray(result);
+      JSONObject breedsObject = array.getJSONObject(0);
+      String breeds = breedsObject.optString("message");
+      breeds = "[" + breeds + "]";
+
+      JSONArray breedsArray = new JSONArray(breeds);
+      JSONObject object = breedsArray.getJSONObject(0);
+      String new_breeds = object.keySet().toString();
+      System.out.println(new_breeds);
       
-      // Test the response
-      System.out.println(response.getStatus());
-      System.out.println(response.getBody().toString());
+      //System.out.println(breeds);
       
     }
 
